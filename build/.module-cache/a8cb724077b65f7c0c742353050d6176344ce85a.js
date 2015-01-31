497e38e8db53f81f798d21bd095b0bcd8f517f7f
Jesse Jurman <j.r.jurman@gmail.com>
2015-01-31 15:59:24 -0500

    first commit, react stuff

---
+++ build/.module-cache/a8cb724077b65f7c0c742353050d6176344ce85a.js
@@ -0,0 +1,5 @@
+React.render(
+  React.createElement("h1", null, "Hello World!"),
+  document.getElementById('example')
+);
+
---
+++ src/helloworld.js
@@ -0,0 +1,5 @@
+React.render(
+  <h1>Hello World!!!</h1>,
+  document.getElementById('example')
+);
+
